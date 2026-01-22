132586db740866d73e437b2223ee2968426a8f77
sumit <stepsmatter8@gmail.com>
2026-01-22 16:11:40 +0530

    update files

--- day2.js
+++ day2.js
@@ -3,21 +3,21 @@
 // }
 // console.log(calc(9,5))
 
-    // const calc = (a , b) =>{
-    //     return a * b
-    // }
-    // console.log(calc(3,5));
-    // const users = ["amit","sumit","deepak","narinder","kuldeep"]
-    // const newUsers = users.map((user)=>{
-    // return "Mr" + user;
-    // })
-    // console.log(newUsers);
+//     const calc = (a , b) =>{
+//         return a * b
+//     }
+//     console.log(calc(3,5));
+//     const users = ["amit","sumit","deepak","narinder","kuldeep"]
+//     const newUsers = users.map((user)=>{
+//     return "Mr" + user;
+//     })
+//     console.log(newUsers);
 
-    // const multiply = [ 2,3,4,5]
-    // const newMultiply = multiply.map((number)=>{
-    //     return 10 * number
-    // })
-    // console.log(newMultiply);
+//     const multiply = [ 2,3,4,5]
+//     const newMultiply = multiply.map((number)=>{
+//         return 10 * number
+//     })
+//     console.log(newMultiply);
     
 
 // const divide = [ 5,7,9,40]
---
+++ day4.js
@@ -0,0 +1,26 @@
+// const obj ={
+//     name : "sumit",
+//     age : 21
+// }
+// const{ name , age} = obj
+// console.log(name);
+// console.log(name);
+
+// const color = [ "pink","red","yellow","black"]
+//  const [ kala , pila , neela] = color
+//  console.log(kala);
+//  console.log(pila);
+//  console.log(neela);
+ 
+// const cities = {
+//     state : 'punjab',
+//     city : 'mohali'
+// }
+// const { state , city} = cities
+// console.log(state);
+// console.log(city);
+
+const fruits = [ "apple", "mango", "kiwi", "banana"]
+const [ first , second ] = fruits
+console.log(first);
+console.log(second);
