8c97a3c259cd9e53a816796b7e1884a583263c9c
sumit <stepsmatter8@gmail.com>
2026-01-24 12:30:09 +0530

    updated files

--- day5.js
+++ day5.js
@@ -4,14 +4,39 @@
 // const merge = [ ...arr,...arr2]
 // console.log(merge);
 
+const { useState } = require("react");
+
 // const arr = ["sumit singh", " sumitsingh@email.com" ]
 // const details = [ ...arr, "age : 26"]
 // console.log(details);
-const students = {
-    name : "sumit singh",
-    email : " sumitsingh@gmail.com"
-}
-const add_moredata = {
-    ...students, age : 20
-}
+// const students = {
+//     name : "sumit singh",
+//     email : " sumitsingh@gmail.com"
+// }
+// const add_moredata = {
+//     ...students, age : 20
+// }
+
+
+// const student = {
+//     name : " sumit singh",
+//     age : 20,
+//     rollno : 2429725
+// }
+// const new_data ={
+//     ...student, name: "rahul chaurasiya"
+// }
+// const { name , rollno} =new_data
+// console.log(name);
+// console.log(rollno);
+
+const [form , setForm ] = useState({
+    email : "",
+    number : ""
+})
+ const handleChange = (e) =>{
+    setForm({
+        ...form,[e.target.name] : e.target.value
+    })
+ }
 
