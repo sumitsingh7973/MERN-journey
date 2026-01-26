1806eaa98e64e228a930409df90630d52a9e7656
sumit <stepsmatter8@gmail.com>
2026-01-26 13:17:06 +0530

    updated-files

--- day7.js
+++ day7.js
@@ -9,19 +9,31 @@
 // }
 // obj.intro();
 
-const student = {
-    name : "sumit",
-    age : 17,
-    isLoggedIn : true
-}
-const updateData = {
-    ...student, age:18,city:"mohali"
-}
- if(updateData.age >= 18,student.isLoggedIn==true){
-    console.log("you are allowed");
-}else{
-    console.log("you are under age")
-}
-console.log(updateData);
+// const student = {
+//     name : "sumit",
+//     age : 17,
+//     isLoggedIn : true
+// }
+// const updateData = {
+//     ...student, age:18,city:"mohali"
+// }
+//  if(updateData.age >= 18,student.isLoggedIn==true){
+//     console.log("you are allowed");
+// }else{
+//     console.log("you are under age")
+// }
+// console.log(updateData);
 
 
+const stn ={
+    name : "kavya",
+    subject : " hindi",
+    city : "mohali",
+    parents : {
+        fatherName :"harendra singh",
+        motherName : "vimla devi"
+    }
+}
+
+console.log(stn.parents.fatherName);
+console.log(stn.parents.motherName);
