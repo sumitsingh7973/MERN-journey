856cacdf991bef62785ff001a995fae9e31b8083
sumit <stepsmatter8@gmail.com>
2026-02-01 14:35:15 +0530

    updated-files

---
+++ js-basics/index.js
@@ -0,0 +1,49 @@
+import express from 'express'
+
+const app = express();
+
+app.get('/api/products', (req, res) => {
+    const products = [
+        {
+            "id": 1,
+            "name": "Burger",
+            "price": 120,
+            "image": "https://example.com/images/burger.jpg"
+        },
+        {
+            "id": 2,
+            "name": "Pizza",
+            "price": 250,
+            "image": "https://example.com/images/pizza.jpg"
+        },
+        {
+            "id": 3,
+            "name": "Pasta",
+            "price": 180,
+            "image": "https://example.com/images/pasta.jpg"
+        },
+        {
+            "id": 4,
+            "name": "Sandwich",
+            "price": 100,
+            "image": "https://example.com/images/sandwich.jpg"
+        },
+        {
+            "id": 5,
+            "name": "Cold Coffee",
+            "price": 90,
+            "image": "https://example.com/images/coffee.jpg"
+        }
+    ]
+
+    setTimeout(() => {
+        res.send(products);
+    }, 3000)
+
+})
+
+const port = process.env.PORT || 3000
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+})
