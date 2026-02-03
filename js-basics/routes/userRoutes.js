7f9689d100db12119554b653e8dacc73251a6582
sumit <stepsmatter8@gmail.com>
2026-02-03 16:32:50 +0530

    updated-files

--- js-basics/routes/userRoutes.js
+++ js-basics/routes/userRoutes.js
@@ -2,3 +2,8 @@ const express = require('express')
 const router = express.Router();
 
 const users = require("../data/users")
+router.get('/users', (req,res)=>{
+    res.json(users)
+})
+
+module.exports = router
--- js-basics/server.js
+++ js-basics/server.js
@@ -1,8 +1,11 @@
 const express = require ('express')
 const app = express()
 
+const userRoutes = require('./routes/userRoutes')
+
+app.use(userRoutes)
 app.get('/',(req,res)=>{
-    res.send("server chal ra h ")
+    res.send("server is runnig")
 })
 
 app.listen(5000,()=>{
