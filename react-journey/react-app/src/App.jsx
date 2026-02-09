328284a5a93b894352e528398213ce1a93523a02
sumit <stepsmatter8@gmail.com>
2026-02-09 16:48:41 +0530

    updated-files

--- react-journey/react-app/src/App.jsx
+++ react-journey/react-app/src/App.jsx
@@ -1,46 +1,10 @@
-import React from 'react'
-// import { useState, useEffect } from 'react'
-import UserForm from './components/UserForm'
+import { useEffect , useState } from "react";
+import Userform from "./components/UserForm";
 
-function App  ()  {
-  // const[users, setUsers] = useState([]);
-  // const[loading, setLoading] = useState(true);
-  // const[error, setError] = useState(null);
-  // useEffect(()=>{
-  //   fetch('https://jsonplaceholder.typicode.com/users')
-  //   .then(response =>{
-  //     if(!response.ok){throw new Error("Data load nhi hua")
-  //     }
-  //   return response.json()
-  //   })
-  //   .then(data =>{
-  //     setUsers(data);
-  //     setLoading(false)
-  //   });
-  // },[])
+function app (){
+  const [ users , setUsers] = useState([]);
 
-  // if(loading){
-  //   return <h2>loading...</h2>
-  // }
-
-  // if(error){
-  //   return <h2>error: {error}</h2>
-  // }
-
-  // return (
-  //   <div>
-  //     <h1>user list</h1>
-  //     {users.map(user=>( <p key = {user.id}>{user.name}</p>))}
-  //   </div>
-  // )
-  return(
-    <div>
-      <h1>post</h1>
-      <UserForm/>
-  </div>
-  )
-  
-
+  useEffect (()=>{
+    fetch("https://jsonplaceholder")
+  })
 }
-
-export default App
