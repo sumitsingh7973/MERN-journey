00068294d3cacc50c0d1460922f89064bd5f62db
sumit <stepsmatter8@gmail.com>
2026-02-11 16:08:46 +0530

    'updated-files'

--- react-journey/react-app/src/App.jsx
+++ react-journey/react-app/src/App.jsx
@@ -1,11 +1,14 @@
+import Counter from "./components/Counter";
 import ToggleText from "./components/ToggleText";
 
 function App() {
   return(
     <div>
       <ToggleText/>
+      <Counter/>
     </div>
   )
-}
+}  
+
 
 export default App
