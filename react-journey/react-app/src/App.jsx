c5249e5c6189c39fd9227ba5917d1eeb2eb72b9d
sumit <stepsmatter8@gmail.com>
2026-02-12 15:36:38 +0530

    'updated-files'

--- react-journey/react-app/src/App.jsx
+++ react-journey/react-app/src/App.jsx
@@ -1,14 +1,14 @@
-import Counter from "./components/Counter";
-import ToggleText from "./components/ToggleText";
+import React from 'react'
 
-function App() {
-  return(
+const App = () => {
+  return (
     <div>
-      <ToggleText/>
-      <Counter/>
+      <div className="card">
+        <h1>card</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo, necessitatibus.</p>
+      </div>
     </div>
   )
-}  
-
+}
 
 export default App
