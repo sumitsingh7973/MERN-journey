03d9a25f8b4b73aef0cf4e380fc93fc2073eddfa
sumit <stepsmatter8@gmail.com>
2026-02-12 17:25:47 +0530

    'updated-files'

--- react-journey/react-app/src/App.jsx
+++ react-journey/react-app/src/App.jsx
@@ -5,8 +5,8 @@ import Navbar from './components/Navbar'
 const App = () => {
   return (
    <div className="parent">
-    <Card/>
-    
+    <Card users = "ananya" bio = "i am ananya saini working as a team leader in netsterz" />
+    <Card users = "sumanya" bio = " hey there i am sumanya i am not a real person right now but someday i will born as a cute baby"/>
    </div>
    
   )
--- react-journey/react-app/src/components/Card.jsx
+++ react-journey/react-app/src/components/Card.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 
-const Card = () => {
+const Card = (props) => {
+ 
+  
   return (
       <div className="card">
         <img src="https://images.unsplash.com/photo-1770296877116-c4973c84d339?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
-        <h1>Sumit Singh</h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+        <h1>{props.users}</h1>
+        <p>{props.bio}</p>
         <button>ok</button>
       </div>
   )
