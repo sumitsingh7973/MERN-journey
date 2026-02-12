a6cb5baf9a83c2bfdb0a15de9260b5676d3c2f5a
sumit <stepsmatter8@gmail.com>
2026-02-12 16:47:22 +0530

    'updated-files'

--- react-journey/react-app/src/App.jsx
+++ react-journey/react-app/src/App.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import Card from './components/Card'
+import Navbar from './components/Navbar'
 
 const App = () => {
   return (
-    <div>
-      <Card/>
-    </div>
+   <div className="parent">
+    <Card/>
+    
+   </div>
+   
   )
 }
 
--- react-journey/react-app/src/components/Card.jsx
+++ react-journey/react-app/src/components/Card.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 
 const Card = () => {
   return (
-    <div>
-      <div className='card'>
-        <h1>hey i am a card</h1>
-        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iusto ut accusamus, eum odit minima nemo?</p>
+      <div className="card">
+        <img src="https://images.unsplash.com/photo-1770296877116-c4973c84d339?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+        <h1>Sumit Singh</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+        <button>ok</button>
       </div>
-    </div>
   )
 }
 
