d712fbbda3c6927a7375b9d8211c533a6966f9e3
sumit <stepsmatter8@gmail.com>
2026-02-14 12:48:42 +0530

    'updated-files'

--- react-journey/react-app/src/App.jsx
+++ react-journey/react-app/src/App.jsx
@@ -1,17 +1,34 @@
 import React from 'react'
-import Card from './components/Card'
-import Navbar from './components/Navbar'
-
+import {Bookmark} from 'lucide-react';
 const App = () => {
   return (
-   <div className="parent">
-    <Card users = "ananya" bio = "i am ananya saini working as a team leader in netsterz" button ="ok" img ="https://images.unsplash.com/photo-1761839271800-f44070ff0eb9?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"/>
+    <div>
+      <div className="parent">
 
-    <Card users = "sumanya" bio = " hey there i am sumanya i am not a real person right now but someday i will born as a cute baby" button="hehehe" img ="https://images.unsplash.com/photo-1770296877116-c4973c84d339?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"/>
-    
-    <Card users = "Sumit" bio = " hey there i am sumit singh i am doing all this to build my future " button="yes" img ="https://images.unsplash.com/photo-1770754288999-c545fc028b18?q=80&w=737&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"/>
-   </div>
-   
+        <div className="card">
+          <div className="top">
+            <img src="https://1000logos.net/wp-content/uploads/2016/10/Amazon-logo-meaning.jpg" alt="" />
+           <button>Save <Bookmark size={15} color="black" /></button>
+          </div>
+          <div className="center">
+            <h2>Amazon <span>5days ago</span></h2>
+            <h2>Senior UI?UX Designer</h2>
+            <div>
+              <h4>Part Time</h4>
+              <h4>Senior Level</h4>
+            </div>
+          </div>
+          <div className="bottom">
+              <div>
+                <h3>$120/hr</h3>
+                <p>Mumbai,India</p>
+              </div>
+              <button>Apply Now</button>
+          </div>
+        </div>
+      </div>
+    </div>
+
   )
 }
 
