5cf4d6ad81673eddc3f16701271e63de5d5ccf09
sumit <stepsmatter8@gmail.com>
2026-02-14 13:45:06 +0530

    'updated-files'

--- react-journey/react-app/src/App.jsx
+++ react-journey/react-app/src/App.jsx
@@ -6,18 +6,89 @@ const App = () => {
       <div className="parent">
 
         <div className="card">
-          <div className="top">
+        <div>
+            <div className="top">
             <img src="https://1000logos.net/wp-content/uploads/2016/10/Amazon-logo-meaning.jpg" alt="" />
            <button>Save <Bookmark size={15} color="black" /></button>
           </div>
           <div className="center">
             <h2>Amazon <span>5days ago</span></h2>
             <h2>Senior UI?UX Designer</h2>
-            <div>
+            <div className='tag'>
               <h4>Part Time</h4>
               <h4>Senior Level</h4>
             </div>
           </div>
+        </div>
+          <div className="bottom">
+              <div>
+                <h3>$120/hr</h3>
+                <p>Mumbai,India</p>
+              </div>
+              <button>Apply Now</button>
+          </div>
+        </div>
+        <div className="card">
+        <div>
+            <div className="top">
+            <img src="https://1000logos.net/wp-content/uploads/2016/10/Amazon-logo-meaning.jpg" alt="" />
+           <button>Save <Bookmark size={15} color="black" /></button>
+          </div>
+          <div className="center">
+            <h2>Amazon <span>5days ago</span></h2>
+            <h2>Senior UI?UX Designer</h2>
+            <div className='tag'>
+              <h4>Part Time</h4>
+              <h4>Senior Level</h4>
+            </div>
+          </div>
+        </div>
+          <div className="bottom">
+              <div>
+                <h3>$120/hr</h3>
+                <p>Mumbai,India</p>
+              </div>
+              <button>Apply Now</button>
+          </div>
+        </div>
+        <div className="card">
+        <div>
+            <div className="top">
+            <img src="https://1000logos.net/wp-content/uploads/2016/10/Amazon-logo-meaning.jpg" alt="" />
+           <button>Save <Bookmark size={15} color="black" /></button>
+          </div>
+          <div className="center">
+            <h2>Amazon <span>5days ago</span></h2>
+            <h2>Senior UI?UX Designer</h2>
+            <div className='tag'>
+              <h4>Part Time</h4>
+              <h4>Senior Level</h4>
+            </div>
+          </div>
+        </div>
+          <div className="bottom">
+              <div>
+                <h3>$120/hr</h3>
+                <p>Mumbai,India</p>
+              </div>
+              <button>Apply Now</button>
+          </div>
+        </div>
+        <div className="card">
+        <div>
+            <div className="top">
+            <img src="https://1000logos.net/wp-content/uploads/2016/10/Amazon-logo-meaning.jpg" alt="" />
+           <button>Save <Bookmark size={15} color="black" /></button>
+          </div>
+          <div className="center">
+            <h2>Amazon <span>5days ago</span></h2>
+            <h2>Senior UI?UX Designer</h2>
+            <div className='tag'>
+              <h4>Part Time</h4>
+              <h4>Senior Level</h4>
+            </div>
+          </div>
+        </div>
           <div className="bottom">
               <div>
                 <h3>$120/hr</h3>
