d6ee16a288103d844ef3997646e4c072523acba1
sumit <stepsmatter8@gmail.com>
2026-02-15 14:46:37 +0530

    'updated-files'

--- react-journey/react-app/src/App.jsx
+++ react-journey/react-app/src/App.jsx
@@ -1,102 +1,22 @@
 import React from 'react'
-import {Bookmark} from 'lucide-react';
+
+import Card from './components/Card';
 const App = () => {
   return (
     <div>
       <div className="parent">
+        <Card company = "Amazon" img="https://1000logos.net/wp-content/uploads/2016/10/Amazon-logo-meaning.jpg" time="5 Days Ago," position="Senior UI/UX Designer" avalability="Senior Level"/>
 
-        <div className="card">
-        <div>
-            <div className="top">
-            <img src="https://1000logos.net/wp-content/uploads/2016/10/Amazon-logo-meaning.jpg" alt="" />
-           <button>Save <Bookmark size={15} color="black" /></button>
-          </div>
-          <div className="center">
-            <h2>Amazon <span>5days ago</span></h2>
-            <h2>Senior UI?UX Designer</h2>
-            <div className='tag'>
-              <h4>Part Time</h4>
-              <h4>Senior Level</h4>
-            </div>
-          </div>
-        </div>
-          <div className="bottom">
-              <div>
-                <h3>$120/hr</h3>
-                <p>Mumbai,India</p>
-              </div>
-              <button>Apply Now</button>
-          </div>
-        </div>
-        <div className="card">
-        <div>
-            <div className="top">
-            <img src="https://1000logos.net/wp-content/uploads/2016/10/Amazon-logo-meaning.jpg" alt="" />
-           <button>Save <Bookmark size={15} color="black" /></button>
-          </div>
-          <div className="center">
-            <h2>Amazon <span>5days ago</span></h2>
-            <h2>Senior UI?UX Designer</h2>
-            <div className='tag'>
-              <h4>Part Time</h4>
-              <h4>Senior Level</h4>
-            </div>
-          </div>
-        </div>
-          <div className="bottom">
-              <div>
-                <h3>$120/hr</h3>
-                <p>Mumbai,India</p>
-              </div>
-              <button>Apply Now</button>
-          </div>
-        </div>
-        <div className="card">
-        <div>
-            <div className="top">
-            <img src="https://1000logos.net/wp-content/uploads/2016/10/Amazon-logo-meaning.jpg" alt="" />
-           <button>Save <Bookmark size={15} color="black" /></button>
-          </div>
-          <div className="center">
-            <h2>Amazon <span>5days ago</span></h2>
-            <h2>Senior UI?UX Designer</h2>
-            <div className='tag'>
-              <h4>Part Time</h4>
-              <h4>Senior Level</h4>
-            </div>
-          </div>
-        </div>
-          <div className="bottom">
-              <div>
-                <h3>$120/hr</h3>
-                <p>Mumbai,India</p>
-              </div>
-              <button>Apply Now</button>
-          </div>
-        </div>
-        <div className="card">
-        <div>
-            <div className="top">
-            <img src="https://1000logos.net/wp-content/uploads/2016/10/Amazon-logo-meaning.jpg" alt="" />
-           <button>Save <Bookmark size={15} color="black" /></button>
-          </div>
-          <div className="center">
-            <h2>Amazon <span>5days ago</span></h2>
-            <h2>Senior UI?UX Designer</h2>
-            <div className='tag'>
-              <h4>Part Time</h4>
-              <h4>Senior Level</h4>
-            </div>
-          </div>
-        </div>
-          <div className="bottom">
-              <div>
-                <h3>$120/hr</h3>
-                <p>Mumbai,India</p>
-              </div>
-              <button>Apply Now</button>
-          </div>
-        </div>
+        <Card company="Flipcart" img= "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQVWzGxiYGlEM-IzG4PWRrn875F0LOcXLulhQ&s" time="1 Day Ago," position="Senior Developer"/>
+
+        <Card company="Myntra" img="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTZ2aVKHUduja9219ut8P9CFqhpJMjIk5dqog&s" time=" 4 Days Ago," position="Seo Executive"/>
+
+        <Card company="Saragarhi securities" img ="https://media.licdn.com/dms/image/v2/D4E0BAQF8NJEyu9NKfA/company-logo_200_200/company-logo_200_200/0/1694771928281?e=2147483647&v=beta&t=jSsKGrzEG07_q4JBfpiqTWIEVoJIByCJRFAAMLhedv8" time="7 Days Ago," position="Field Officer"/>
+
+        <Card company="Meesho" img="https://images.moneycontrol.com/static-mcnews/2023/06/Meesho-682x435.jpg?impolicy=website&width=1600&height=900" time="2 Days Ago," position="Telecaller"/>
+
+        <Card company="Blinkit" img="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRjBgWsrhwRuIESGkEdGkw_F3ocQGsY_8HZFQ&s" time="6 Days Ago," position="Delivery Partner" avalability="fulltime"/ >
+
       </div>
     </div>
 
--- react-journey/react-app/src/components/Card.jsx
+++ react-journey/react-app/src/components/Card.jsx
@@ -1,15 +1,33 @@
 import React from 'react'
-
+import {Bookmark} from 'lucide-react';
 const Card = (props) => {
+ ;
  
   
   return (
-      <div className="card">
-        <img src={props.img} alt="" />
-        <h1>{props.users}</h1>
-        <p>{props.bio}</p>
-        <button>{props.button}</button>
-      </div>
+     <div className="card">
+        <div>
+            <div className="top">
+            <img src={props.img} alt="" />
+           <button>Save <Bookmark size={15} color="black" /></button>
+          </div>
+          <div className="center">
+            <h2>{props.company} <span>{props.time}</span></h2>
+            <h2>{props.position}</h2>
+            <div className='tag'>
+              <h4>Part Time</h4>
+              <h4>{props.avalability}</h4>
+            </div>
+          </div>
+        </div>
+          <div className="bottom">
+              <div>
+                <h3>$120/hr</h3>
+                <p>Mumbai,India</p>
+              </div>
+              <button>Apply Now</button>
+          </div>
+        </div>
   )
 }
 
