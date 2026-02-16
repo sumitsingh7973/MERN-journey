897e4d3ffd271ae60a73615d3771bab4cc2aab10
sumit <stepsmatter8@gmail.com>
2026-02-16 16:36:43 +0530

    'updated-files'

--- react-journey/react-app/src/App.jsx
+++ react-journey/react-app/src/App.jsx
@@ -4,15 +4,10 @@ import Card from './components/Card';
 import User from './components/User';
 const App = () => {
 
-  const arr = [10,20,30,40]
   return (
     <div>
       <div className="parent">
-        {arr.map(function(naam){
-          console.log(naam);
-          
-          return naam*2
-        })}
+        
       </div>
     </div>
 
