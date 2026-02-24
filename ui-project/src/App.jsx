19f932032b536e3ff5e5b5aadd73f21aaff9658f
sumit <stepsmatter8@gmail.com>
2026-02-24 15:53:20 +0530

    'updated-files'

--- ui-project/src/App.jsx
+++ ui-project/src/App.jsx
@@ -1,34 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import Section1 from './components/Section1/Section1'
+import Section2 from './components/Section2/Section2'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='h-100 w-500 bg-amber-600'>
+      <Section1/>
+      <Section2/>
+    </div>
   )
 }
 
---
+++ ui-project/src/components/Section1/Section1.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Section1 = () => {
+  return (
+    <div className='h-screen w-full bg-green-500'>
+      
+    </div>
+  )
+}
+
+export default Section1
