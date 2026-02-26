0294a2e9fb74950f074caf72d3ce519ffc5f363b
sumit <stepsmatter8@gmail.com>
2026-02-26 17:30:44 +0530

    'updated-files'

--- ui-project/src/components/Section1/LeftContent.jsx
+++ ui-project/src/components/Section1/LeftContent.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
-import {MoveUpRight} from 'lucide-react';
+import {ArrowUpRight} from 'lucide-react';
 
 
 const LeftContent = () => {
   return (
-    <div className='h-full flex  flex-col justify-between w-1/4 bg-blue-200 '>
-          <div className='p-76'>
-            <h3 className='text-5xl bold mb-5'>Prospective <br /><span> Customer</span>  <br />Segmentation</h3>
-            <p className='text-xl'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nemo delectus iure dolore itaque magni numquam deleniti expedita ab quo aut.</p>
+    <div className='h-full flex  flex-col justify-between w-1/3 '>
+          <div className='p-6'>
+            <h3 className='text-5xl font-bold mb-7'>Prospective <br /><span className='text-gray-500 '> customer</span>  <br />Segmentation</h3>
+            <p className='text-xl font-medium text-gray-700'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nemo delectus iure dolore itaque magni numquam deleniti expedita ab quo aut.</p>
           </div>
-         <div className=''>
-        <MoveUpRight  />
+         <div className="">
+        <ArrowUpRight size={44} />
         </div>
     </div>
     
