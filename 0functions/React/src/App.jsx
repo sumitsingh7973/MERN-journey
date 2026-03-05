09d86c8e38265f42d370f0e94a12c61bae5fd052
sumit <stepsmatter8@gmail.com>
2026-03-05 15:24:37 +0530

    'updated-files'

--- 0functions/React/src/App.jsx
+++ 0functions/React/src/App.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
 
 const App = () => {
-  
+  function change(val){
+    console.log(val);
+    
+  }
 
   return (
     <div>
       <h1>hello</h1>
       <input onChange={function(elem){
-        console.log(elem.target.value);
-
+        change(elem.target.value)
       }} type="text"/>
     </div>
   )
