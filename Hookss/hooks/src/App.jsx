4dd4fff131cff310c2a1833dc2d2390295fbf46b
sumit <stepsmatter8@gmail.com>
2026-03-06 12:39:43 +0530

    updated-files

--- Hookss/hooks/src/App.jsx
+++ Hookss/hooks/src/App.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const App = () => {
+  const [num , setNum] = useState(10)
+
+  const btnClick =()=>{
+    setNum(20)
+    
+  }
+
   return (
     <div>
-      
+      <h1>{num}</h1>
+      <button onClick={btnClick}>Click</button>
     </div>
   )
 }
