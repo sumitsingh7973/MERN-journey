bf3826f6c2ef55fc32dcedfd918bd5c6efb5d7f6
sumit <stepsmatter8@gmail.com>
2026-03-14 16:46:02 +0530

    updated-files

--- form-handling/Form-handling/src/App.jsx
+++ form-handling/Form-handling/src/App.jsx
@@ -7,11 +7,11 @@ const App = () => {
     console.log("Form-submitted");
   }
   return (
-    <div className='h-screen  bg-black'>
+    <div className='h-screen lg:flex  bg-black '>
       <form onSubmit={(e)=>{
         submitHandler(e)
-      }}className='flex items-start p-10 justify-between '>
-        <div className=' flex gap-4 items-start flex-col w-1/2 '>
+      }}className='flex flex-col Lg:w-1/2 gap-4 p-10 items-start  '>
+        
 
         <input type="text" 
         placeholder='Enter Task Heading'
@@ -24,9 +24,12 @@ const App = () => {
         className='px-5 w-full h-30 py-2 flex items-start flex-row border-2 outline-none rounded text-white font-medium'
          />
 
-        <button className='bg-white text-black h-10  rounded mt-5 w-full outline-none'>add note</button></div>
-        <img className="h-60 rounded-full"  src="https://img.freepik.com/premium-vector/note-paper-with-paperclip-white_87744-881.jpg?semt=ais_hybrid&w=740&q=80" alt="" />
+        <button className='bg-white text-black h-10  rounded mt-5 w-full outline-none'>add note</button>
+        {/* <img className="h-60 rounded-full"  src="https://img.freepik.com/premium-vector/note-paper-with-paperclip-white_87744-881.jpg?semt=ais_hybrid&w=740&q=80" alt="" /> */}
       </form>
+      <div className='flex flex-wrap lg:w-1/2 p-10 bg-gray-900'>
+        <div className='h-32 w-32 rounded-2xl bg-white'></div>
+      </div>
     </div>
   )
 }
