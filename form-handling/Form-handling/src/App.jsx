37df331ebaf88b5918942a9a34d11ff6c3dab1e1
sumit <stepsmatter8@gmail.com>
2026-03-14 17:03:09 +0530

    updated-files

--- form-handling/Form-handling/src/App.jsx
+++ form-handling/Form-handling/src/App.jsx
@@ -11,8 +11,7 @@ const App = () => {
       <form onSubmit={(e)=>{
         submitHandler(e)
       }}className='flex flex-col Lg:w-1/2 gap-4 p-10 items-start  '>
-        
-
+        <h1 className='text-3xl font-bold'>Add Notes</h1>
         <input type="text" 
         placeholder='Enter Task Heading'
         className='p-5 w-full border-2 h-5 rounded mb-5 outline-none text-white font-medium '
@@ -27,8 +26,20 @@ const App = () => {
         <button className='bg-white text-black h-10  rounded mt-5 w-full outline-none'>add note</button>
         {/* <img className="h-60 rounded-full"  src="https://img.freepik.com/premium-vector/note-paper-with-paperclip-white_87744-881.jpg?semt=ais_hybrid&w=740&q=80" alt="" /> */}
       </form>
-      <div className='flex flex-wrap lg:w-1/2 p-10 bg-gray-900'>
-        <div className='h-32 w-32 rounded-2xl bg-white'></div>
+      <div className=' lg:w-1/2  p-10 lg:border-l-2 border-white '>
+        <h1 className='text-3xl font-bold text-white'>Your notes</h1>
+        <div className='flex flex-wrap gap-5 h-full mt-5 overflow-auto'>
+          <div className='h-50 w-40 rounded-2xl bg-white'></div>
+          <div className='h-50 w-40 rounded-2xl bg-white'></div>
+          <div className='h-50 w-40 rounded-2xl bg-white'></div>
+          <div className='h-50 w-40 rounded-2xl bg-white'></div>
+          <div className='h-50 w-40 rounded-2xl bg-white'></div>
+          <div className='h-50 w-40 rounded-2xl bg-white'></div>
+          <div className='h-50 w-40 rounded-2xl bg-white'></div>
+          <div className='h-50 w-40 rounded-2xl bg-white'></div>
+          <div className='h-50 w-40 rounded-2xl bg-white'></div>
+          <div className='h-50 w-40 rounded-2xl bg-white'></div>
+        </div>
       </div>
     </div>
   )
