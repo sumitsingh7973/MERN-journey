ad53115fdd01200dfe04ab89edf25405b3f5cb06
sumit <stepsmatter8@gmail.com>
2026-03-17 16:26:58 +0530

    updated-files

--- react-hooks/src/App.jsx
+++ react-hooks/src/App.jsx
@@ -3,18 +3,23 @@ import React, { useEffect, useState } from 'react'
 const App = () => {
 
     const [num, setNum] = useState(0)
-    const [num2, setNum2] = useState(10)
+    const [num2, setNum2] = useState(0)
 
     useEffect(function () {
         console.log("useeffect is running")
-    }, [num])
+    }, [num2])
     return (
         <div>
-            <h1>{num}</h1>
-            <h1>value of num2 is {num2}</h1>
+            <h1>value of num is{num}</h1>
+            <h2>value of num2 is {num2}</h2>
             <button onClick={() => {
-            setNum(num + 1) 
+                setNum(num + 1)
             }}>click</button>
+            <button onClick={() => {
+                setNum2(num2- 1)
+            }}>
+                decrease
+            </button>
         </div>
     )
 }
