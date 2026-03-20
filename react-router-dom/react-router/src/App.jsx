c6d0169ad60201fde9bdc97ee94b9072683713bd
sumit <stepsmatter8@gmail.com>
2026-03-20 17:10:48 +0530

    update-files

--- react-router-dom/react-router/src/App.jsx
+++ react-router-dom/react-router/src/App.jsx
@@ -6,17 +6,21 @@ import About from './pages/About'
 
 const App = () => {
   return (
-    <div>
-      <div>
-        <h2>This is navbar</h2>
+    <div className='bg-gray-600 h-screen'>
+      <div className='flex justify-end gap-5 items-center p-5 text-white font-semibold text-lg'>
+        <h1>Home</h1>
+        <h1>Contact</h1>
+        <h1>About Us</h1>
       </div>
-      <div>
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/about' element={<About/> }/>
-      </Routes>
+
+      <div className='border-t border-white flex justify-between'>
+        <div className='min-h-150 min-w-140 m-2 '>
+          <h2 className='font-bold font-serif pt-5' >This is Website Which i've Created</h2>
+          <p className=''>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur, repudiandae rerum? Rerum nihil nam, quisquam dignissimos fuga quasi tempore obcaecati omnis. Aperiam possimus placeat sequi?</p>
+        </div>
+        <div className='bg-black w-full m-2 '></div>
       </div>
-      
+      <div>footer</div>
     </div>
   )
 }
