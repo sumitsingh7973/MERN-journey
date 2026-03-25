fe73ae291ef0cfdd61aa6696506acb78d2915838
sumit <stepsmatter8@gmail.com>
2026-03-25 18:04:02 +0530

    updated-files

--- Routing-advanced/Advance-routing/src/pages/About.jsx
+++ Routing-advanced/Advance-routing/src/pages/About.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const About = () => {
+
+     const navigate =useNavigate()
     return (
         <div>
+            <button onClick={()=>{
+                navigate('/')
+            }}
+            className='bg-emerald-900 text-white px-5 py-5 rounded cursor-pointer m-5 active:scale-90 '>Return to home-page</button>
+
+            <button onClick={()=>{
+                navigate(-1)
+            }}
+            className='bg-emerald-900 text-white px-5 py-5 rounded cursor-pointer m-5 active:scale-90 '>Return  Back</button>
             <h1>About</h1>
         </div>
     )
