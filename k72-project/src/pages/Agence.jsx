5d97368618516956b4d3a5872d69c7398d2e0370
sumit <stepsmatter8@gmail.com>
2026-04-03 18:23:25 +0530

    updated-files

--- k72-project/src/pages/Agence.jsx
+++ k72-project/src/pages/Agence.jsx
@@ -2,8 +2,11 @@ import React from 'react'
 
 const Agence = () => {
   return (
-    <div>
-      <h2>i am agnece</h2>
+    <div className='font-[font2]'>
+      <div className=' mt-[55vh]'>
+        <h1 className='text-[20vw] text-center uppercase leading-[17vw]'>SEVEN7Y<br/>
+            TWO</h1>
+      </div>
     </div>
   )
 }
