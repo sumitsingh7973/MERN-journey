11d5d18205763bb47dcf321af8da55e97c60b667
sumit <stepsmatter8@gmail.com>
2026-04-03 18:16:45 +0530

    updated-files

--- k72-project/src/components/home/HomeBottomText.jsx
+++ k72-project/src/components/home/HomeBottomText.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const HomeBottomText = () => {
   return (
-    <div>
-
+    <div className='font-[font2] flex justify-center gap-3 items-center'>
+      <div className='border-3 h-44 hover:border-[#D3FD50] hover:text-[#D3FD50] flex items-center px-14 border-white rounded-full uppercase'>
+        <Link className='text-[6vw] mt-6'to='/Projects'>Projects</Link>
+      </div>
+      <div className='border-3 h-44 hover:border-[#D3FD50] hover:text-[#D3FD50] flex items-center px-14 border-white rounded-full uppercase'>
+        <Link className='text-[6vw] mt-6' to= '/Agence'>Agence</Link>
+      </div>
     </div>
   )
 }
--- k72-project/src/components/home/HomeHeroText.jsx
+++ k72-project/src/components/home/HomeHeroText.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import Video from './Video'
 
 const HomeHeroText = () => {
   return (
-    <div>
-      
+    <div className='font-[font1] pt-5 text-center'>
+      <div className='text-[10vw] uppercase leading-[9.5vw] justify-center flex'>The spark for</div>
+      <div className='text-[10vw] uppercase leading-[9.5vw] justify-center flex'>
+        all
+        <div className='h-[8vw] rounded-full overflow-hidden mt-3 '>
+          <Video />
+        </div>things
+      </div>
+      <div className='text-[10vw] uppercase leading-[9.5vw] justify-center flex'>
+        creative
+      </div>
     </div>
   )
 }
