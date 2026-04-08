6dfe02c890c0ee5b03e1827bcff3aeefb98e7863
sumit <stepsmatter8@gmail.com>
2026-04-08 18:12:19 +0530

    updated-files

--- k72-project/src/components/home/Video.jsx
+++ k72-project/src/components/home/Video.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 
+
 const Video = () => {
   return (
     <div className='h-full w-full'>
-      <video className='h-full w-full object-cover' autoPlay loop muted src="https://www.pexels.com/download/video/28649481/"></video>
+      <video className='h-full w-full object-cover' autoPlay loop muted src='https://download-video-ak.vimeocdn.com/v3-1/playback/36bc59b8-6671-4358-abc2-15555fc6ae59/69496b2d?__token__=st=1775638875~exp=1775642475~acl=%2Fv3-1%2Fplayback%2F36bc59b8-6671-4358-abc2-15555fc6ae59%2F69496b2d%2A~hmac=7e527530af1359535a5c75d832c3b4af3886679d8e5419c3ff8f1308b6752828&r=dXMtZWFzdDE%3D'></video>
     </div>
   )
 }
--- k72-project/src/pages/Agence.jsx
+++ k72-project/src/pages/Agence.jsx
@@ -32,7 +32,6 @@ const Agence = () => {
     gsap.to(imageDivRef.current, {
       scrollTrigger: {
         trigger: imageDivRef.current,
-        markers: true,
         start: 'top 25%',
         end: 'top -70%',
         pin: true,
