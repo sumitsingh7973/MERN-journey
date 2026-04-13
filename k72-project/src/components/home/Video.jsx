577ca5f36a58b704950f5d11c99dab205b173eb5
sumit <stepsmatter8@gmail.com>
2026-04-13 22:30:04 +0530

    'updated-files'

--- k72-project/src/components/home/Video.jsx
+++ k72-project/src/components/home/Video.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 const Video = () => {
   return (
     <div className='h-full w-full'>
-      <video className='h-full w-full object-cover' autoPlay loop muted src='https://www.pexels.com/download/video/35431768/'></video>
+      <video className='h-full w-full object-cover' autoPlay loop muted src='https://static.videezy.com/system/resources/previews/000/039/317/original/17_036_03.mp4'></video>
     </div>
   )
 }
