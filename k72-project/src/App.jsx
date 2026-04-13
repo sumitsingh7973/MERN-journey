ad7fa25cc7cdf1911008bc2a75636a85c6b56f8b
sumit <stepsmatter8@gmail.com>
2026-04-13 22:06:32 +0530

    'updated-files'

--- k72-project/src/App.jsx
+++ k72-project/src/App.jsx
@@ -2,6 +2,7 @@ import Home from './pages/Home'
 import Projects from './pages/Projects'
 import Agence from './pages/Agence'
 import { Route, Routes } from 'react-router-dom'
+import Navbar from './components/navigation/Navbar'
 
 
 const App = () => {
@@ -9,6 +10,7 @@ const App = () => {
 
   return (
     <div>
+      <Navbar/>
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/Projects' element={<Projects />} />
---
+++ k72-project/src/components/Navigation/Navbar.jsx
@@ -0,0 +1,12 @@
+const Navbar = () => {
+  return (
+    <div className='z-10 flex fixed top-0 w-full items-start justify-between'>
+      <svg xmlns="http://www.w3.org/2000/svg" width="103" height="44" viewBox="0 0 103 44">
+        <path fillRule="evenodd" d="M35.1441047,8.4486911 L58.6905011,8.4486911 L58.6905011,-1.3094819e-14 L35.1441047,-1.3094819e-14 L35.1441047,8.4486911 Z ..."></path>
+      </svg>
+    </div>
+    
+  )
+}
+
+export default Navbar
