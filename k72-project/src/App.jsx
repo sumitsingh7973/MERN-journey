b32ace2f3f4459dcb165181211e4ccdc313b0656
sumit <stepsmatter8@gmail.com>
2026-04-15 16:48:03 +0530

    'updated-files'

--- k72-project/src/App.jsx
+++ k72-project/src/App.jsx
@@ -10,7 +10,7 @@ const App = () => {
 
   return (
     <div>
-      <Navbar/>
+      <Navbar />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/Projects' element={<Projects />} />
