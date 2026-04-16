c7e489cd583d94367b0d25d63691d4edf9e07d5c
sumit <stepsmatter8@gmail.com>
2026-04-16 17:54:12 +0530

    'updated-files'

--- k72-project/src/components/Navigation/FullScreenNav.jsx
+++ k72-project/src/components/Navigation/FullScreenNav.jsx
@@ -5,21 +5,15 @@ const FullScreenNav = () => {
     <div id='fullscreennav' className='h-screen px-5 w-full absolute bg-amber-900'>
       <div className='all-links mt-[10vw]'>
         <div className='link border-y'>
-          <h1 className='font-[font] text-[8vw] leading-[0.8] pt-2 uppercase'>Project</h1>
-          {/* <div>
-            <div>
-              <h2>WORK</h2>
-              <img src="https://k72.ca/images/caseStudies/iA_BRAND/Thumbnail.png?w=1280&h=960&s=008dc81d7a1c8fa45e299f6d2dbafc93" alt="" />
-              <h2>WORK</h2>
+          <h1 className='font-[font] text-[8vw] text-center leading-[0.8] pt-5 uppercase'>WORK</h1>
+          <div className='absolute flex  '>
+            <div className='flex items-center overflow-auto'>
+              <h2 className=' whitespace-nowrap font-[font] text-[8vw] text-center leading-[0.8] pt-5 uppercase'>WORK</h2>
+              <img src="https://k72.ca/images/caseStudies/PJC/Thumbnails/PJC_SiteK72_Thumbnail_1280x960.jpg?w=1280&h=960&s=b5151821a8c0d9603263d7ec827bee9b" alt="" />
+              <h2 className=' whitespace-nowrap font-[font] text-[8vw] text-center leading-[0.8] pt-5 uppercase'>WORK</h2>
               <img src="https://k72.ca/images/caseStudies/WIDESCAPE/WS---K72.ca---Thumbnail.jpg?w=1280&h=960&s=650a04dfc31ad85bfc64c0ddccc83f1e" alt="" />
             </div>
-            <div>
-              <h2>WORK</h2>
-              <img src="https://k72.ca/images/caseStudies/iA_BRAND/Thumbnail.png?w=1280&h=960&s=008dc81d7a1c8fa45e299f6d2dbafc93" alt="" />
-              <h2>WORK</h2>
-              <img src="https://k72.ca/images/caseStudies/WIDESCAPE/WS---K72.ca---Thumbnail.jpg?w=1280&h=960&s=650a04dfc31ad85bfc64c0ddccc83f1e" alt="" />
-            </div>
-          </div> */}
+          </div>
         </div>
       </div>
     </div>
