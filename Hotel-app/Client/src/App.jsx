faa984a78e6598c4f3738bb44dab825cdb60d5c0
sumit <stepsmatter8@gmail.com>
2026-05-19 12:15:41 +0530

    updated-files

--- Hotel-app/Client/src/App.jsx
+++ Hotel-app/Client/src/App.jsx
@@ -1,9 +1,20 @@
+import { Route, Routes, useLocation } from "react-router-dom"
+import Home from "./pages/Home"
+import Navbar from "./components/Navbar"
 
 
 const App = () => {
+
+  const isOwnerPath = useLocation().pathname.includes("owner")
+
   return (
     <div>
-      
+      { !isOwnerPath && <Navbar/>}
+      <div className='min-h-[70vh]'>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+        </Routes>
+      </div>
     </div>
   )
 }
