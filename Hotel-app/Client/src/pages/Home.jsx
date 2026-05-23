8e217eab9cd213481bd3a95443dfee0083a6fa70
sumit <stepsmatter8@gmail.com>
2026-05-23 15:00:28 +0530

    updated-files

--- Hotel-app/Client/src/pages/Home.jsx
+++ Hotel-app/Client/src/pages/Home.jsx
@@ -2,6 +2,7 @@
 import ExclusiveOffers from '../components/ExclusiveOffers'
 import FeaturedDestination from '../components/FeaturedDestination'
 import Hero from '../components/Hero'
+import Testimonial from '../components/Testimonial'
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
       <Hero/>
       <FeaturedDestination/>
       <ExclusiveOffers/>
+      <Testimonial/>
     </>
   )
 }
