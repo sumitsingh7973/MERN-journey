d3a8ed347eafbd1d1dab2657e370323711c45612
sumit <stepsmatter8@gmail.com>
2026-05-24 14:56:32 +0530

    updated-files

--- Hotel-app/Client/src/App.jsx
+++ Hotel-app/Client/src/App.jsx
@@ -1,6 +1,7 @@
 import { Route, Routes, useLocation } from "react-router-dom"
 import Home from "./pages/Home"
 import Navbar from "./components/Navbar"
+import Footer from "./components/Footer"
 
 
 const App = () => {
@@ -15,6 +16,7 @@ const App = () => {
           <Route path="/" element={<Home />} />
         </Routes>
       </div>
+      <Footer/>
     </div>
   )
 }
--- Hotel-app/Client/src/pages/Home.jsx
+++ Hotel-app/Client/src/pages/Home.jsx
@@ -1,6 +1,7 @@
 // import React from 'react'
 import ExclusiveOffers from '../components/ExclusiveOffers'
 import FeaturedDestination from '../components/FeaturedDestination'
+import Footer from '../components/Footer'
 import Hero from '../components/Hero'
 import NewsLetter from '../components/NewsLetter'
 import Testimonial from '../components/Testimonial'
@@ -13,6 +14,7 @@ const Home = () => {
       <ExclusiveOffers/>
       <Testimonial/>
       <NewsLetter/>
+      
     </>
   )
 }
