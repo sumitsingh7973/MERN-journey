db273bc70c26d0f4bd511a35e321f7e87a3d2805
sumit <stepsmatter8@gmail.com>
2026-05-25 17:37:33 +0530

    updated-files

--- Hotel-app/Client/src/App.jsx
+++ Hotel-app/Client/src/App.jsx
@@ -2,6 +2,7 @@ import { Route, Routes, useLocation } from "react-router-dom"
 import Home from "./pages/Home"
 import Navbar from "./components/Navbar"
 import Footer from "./components/Footer"
+import AllRooms from "./pages/AllRooms"
 
 
 const App = () => {
@@ -14,9 +15,10 @@ const App = () => {
       <div className='min-h-[70vh]'>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/rooms" element={<AllRooms />} />
         </Routes>
       </div>
-      <Footer/>
+      <Footer />
     </div>
   )
 }
---
+++ Hotel-app/Client/src/pages/AllRooms.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const AllRooms = () => {
+    return (
+        <div className='flex flex-col-reverse lg:flex-row items-start justify-between pt-28 md:pt-35 px-4 md:px-16 lg:px-24 xl:px-32'>
+            <div>
+                
+            </div>
+        </div>
+    )
+}
+
+export default AllRooms
