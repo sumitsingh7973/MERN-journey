225e7367ed81eda586a63f565ccdf388e773d832
sumit <stepsmatter8@gmail.com>
2026-05-29 14:10:22 +0530

    updated-files

--- Hotel-app/Client/src/App.jsx
+++ Hotel-app/Client/src/App.jsx
@@ -3,6 +3,7 @@ import Home from "./pages/Home"
 import Navbar from "./components/Navbar"
 import Footer from "./components/Footer"
 import AllRooms from "./pages/AllRooms"
+import RoomDetails from "./pages/RoomDetails"
 
 
 const App = () => {
@@ -16,6 +17,7 @@ const App = () => {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/rooms" element={<AllRooms />} />
+          <Route path="/rooms/:id" element={<RoomDetails/>} />
         </Routes>
       </div>
       <Footer />
---
+++ Hotel-app/Client/src/pages/RoomDetails.jsx
@@ -0,0 +1,26 @@
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { roomsDummyData } from '../assets/assets'
+
+const RoomDetails = () => {
+    const {id} = useParams()
+    const [room , setRoom] = useState(null)
+    const [mainImage , setMainImage] = useState(null)
+
+    useEffect(()=>{
+      const room = roomsDummyData.find(room => room._id === id)
+      room && setRoom(room)
+      room && setMainImage(room.images[0])
+    },[])
+
+  return room && (
+    <div className='py-28 md:py-35 px-4 md:px-16 lg:px-24 xl:px-32'>
+      {/* room details */}
+      <div>
+        <h1>{room.hotel.name}<span></span></h1>
+      </div>
+    </div>
+  )
+}
+
+export default RoomDetails
