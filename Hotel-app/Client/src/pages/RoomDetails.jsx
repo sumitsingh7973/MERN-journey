b4c0e648775556989b2a24ef3c08975aa79991a0
sumit <stepsmatter8@gmail.com>
2026-05-31 16:30:18 +0530

    updated-files

--- Hotel-app/Client/src/pages/RoomDetails.jsx
+++ Hotel-app/Client/src/pages/RoomDetails.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
-import { roomsDummyData } from '../assets/assets'
+import { assets, roomsDummyData } from '../assets/assets'
+import StarRating from '../components/StarRating'
 
 const RoomDetails = () => {
     const {id} = useParams()
@@ -17,8 +18,20 @@ const RoomDetails = () => {
     <div className='py-28 md:py-35 px-4 md:px-16 lg:px-24 xl:px-32'>
       {/* room details */}
       <div className='flex flex-col md:flex-row items-start md:items-center gap-2'>
-        <h1 className='text-3xl md:text-4xl font-playfair'>{room.hotel.name}<span className='font-playfair'>({room.roomType})</span></h1>
-        <p>20% off</p>
+        <h1 className='text-3xl md:text-4xl font-playfair'>{room.hotel.name}<span className='font-inter text-sm'>({room.roomType})</span></h1>
+        <p className='text-xs font-inter py-1.5 px-3 text-white bg-orange-500 rounded-full'>20% off</p>
+      </div>
+
+      {/* room rating */}
+      <div>
+        <StarRating/>
+        <p className='ml-2'> 200+ reviews</p>
+      </div>
+
+      {/*Room address */}
+      <div>
+        <img src={assets.locationIcon} alt="location-icon" />
+        <span>{room.hotel.address}</span>
       </div>
     </div>
   )
