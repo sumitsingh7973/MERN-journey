478860959c21c699d2c7193f60d4366c8c0aadc9
sumit <stepsmatter8@gmail.com>
2026-06-05 19:51:22 +0530

    updated-files

--- Hotel-app/Client/src/pages/Mybookings.jsx
+++ Hotel-app/Client/src/pages/Mybookings.jsx
@@ -1,17 +1,44 @@
 import Title from "../components/Title"
+import {assets, userBookingsDummyData} from '../assets/assets'
+import { useState } from "react"
 
 
 const Mybookings = () => {
+
+  const [booking , setBookings] = useState(userBookingsDummyData)
   return (
     <div className="py-28 md:pb-35 md:pt-32 px-4 md:px-16 lg:px-24 xl:px-32">
 
       <Title title='My Bookings' subTitle='Easily your past, current and upcoming hotel reservation in one place. Plan your trips seamlessly with just a few clicks' align='left' />
 
-    <div>
-        <div>
-            
+      <div className="max-w-6xl mt-8 w-full text-gray-800">
+        <div className="hidden md:grid md:grid-cols-[3fr_2fr_1fr] w-full border-b border-gray-300 font-medium text-base py-3">
+          <div className="w-1/3">Hotels</div>
+          <div className="w-1/3">Date & Timings</div>
+          <div className="w-1/3">Payment</div>
         </div>
-    </div>
+        {booking.map((booking)=>(
+          <div key={booking._id} className="grid grid-cols-1 md:grid-cols-[3fr_2fr_1fr] w-full border-b border-gray-300 py-6 first:border-t">
+            {/* Hotels Details */}
+            <div>
+              <img className="md:w-44 rounded shadow object-cover" src={booking.room.images[0]} alt="hotel-img"  />
+              <div>
+                <p className="font-playfair text-2xl">{booking.hotel.name}
+                <span className="font-inter text-sm">({booking.room.roomType})</span>
+                </p>
+                <div className="flex items-center gap-1 text-sm text-gray-500">
+                  <img  src={assets.locationIcon} alt="location-icon"  />
+                  <span>{booking.hotel.address}</span>
+                </div>
+              </div>
+            </div>
+              {/* Bookings & Timings */}
+            <div></div>
+              {/* Payment status */}
+            <div></div>
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
