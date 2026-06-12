cedb0556736a560f8819ca43e1d087e4f5d234fc
sumit <stepsmatter8@gmail.com>
2026-06-12 19:10:29 +0530

    updated-files

--- Hotel-app/Client/src/components/HotelReg.jsx
+++ Hotel-app/Client/src/components/HotelReg.jsx
@@ -3,9 +3,12 @@ import React from 'react'
 const HotelReg = () => {
   return (
     <div className='fixed top-0 bottom-0 left-0 right-0 zoom-100 flex items-center justiy-center bg-blak-'>
-      
+      <form>
+        
+      </form>
     </div>
   )
+  
 }
 
 export default HotelReg
