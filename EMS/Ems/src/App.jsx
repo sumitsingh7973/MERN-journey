88db7358174c11d8e137c6af6896d54d2fb1a3b7
sumit <stepsmatter8@gmail.com>
2026-06-22 18:57:21 +0530

    updated-files

---
+++ EMS/Ems/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='bg-black h-screen'>
+      <p className='text-white'>
+        hey there
+      </p>
+    </div>
+  )
+}
+
+export default App
